3e1c2a89dae4493cb48fb1176bd11eacb68b826f
Phanthat Muenprap <phanthat54@gmail.com>
2022-09-15 14:47:17 +0700

    .

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,7 +5,27 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src="/profileImages/lisa.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">Lisa</span>
+          <br />
+          <span>จริงค่า</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span className="text-muted">999 คน</span>
+          </div>
+        </div>
+      </div>
 
       {/*render Reply here... */}
     </div>
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/pepeTheFrog.jpeg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">Phanthant Muenprap 640610652</span>
+      </div>
+      <span>Quiz ทำไม่ทันค้าบบบ T T #261207</span>
+      <div className="d-flex align-items-center gap-1">
+        <img src="/sad.png" width={20}></img>
+        <span className="text-muted">100 คน</span>
+      </div>
+      <hr className="m-0" />
+    </div>
+  );
 }
